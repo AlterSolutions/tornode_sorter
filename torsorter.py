78cab3dd6e5fdb83d3530ec088aaf50fd3410463
ndiab <nabil.diab59@gmail.com>
2023-06-08 17:45:48 +0000

    reorganization of the code

--- torsorter.py
+++ torsorter.py
@@ -1,44 +1,99 @@
 import requests
 
+####################################
+#### GLOBAL VARIABLE DEFINITION ####
+####################################
+
 # All definition regardiong node objects : https://metrics.torproject.org/onionoo.html
+url = "https://onionoo.torproject.org"
+args = "details?limit=1000"
+full_url = url + '/' + args
 
-url = "https://onionoo.torproject.org/details?limit=100000"
+output_directory = "/srv/tornodes_lists/"
 
-response = requests.get(url)
-
-response_json = dict(response.json())
-#print(response_json)
+raw_response = requests.get(full_url)
+response_json = dict(raw_response.json())
 
 relay_list = response_json["relays"]
 
-# Initialisation of the global lists
-guard_list_ports = {} # dictionary of all guards, ordered by listening ports
-guard_set = set() # set of IP of all guards
 
 
-for node in relay_list:
+
+def extract_ip_port(ip_port_string: str) -> dict:
+    """
+    Parse a string representing an IP:PORT couple
+    detect if it is IPV4 or IPV6 and split it in the right way.
+    Return an IP Port dictionnary :
+        {"IP" : "<IP>"; "PORT": int(<port>)}
+    """
+    ip_port_dict = dict()
+
+    if ip_port_string[0] != '[': # if IPv4
+        ip = ip_port_string.split(':')[0]
+        port = int(ip_port_string.split(':')[1])
+    else: # if IP6v
+        ip = ip_port_string[1:].split(']')[0]
+        port = int(ip_port_string[1:].split(']')[1][1:])
+
+    ip_port_dict["IP"] = ip
+    ip_port_dict["PORT"] = port
+
+    return ip_port_dict
 
 
-    if "Guard" in node["flags"]:
-        try:
-            if node["guard_probability"] != 0:
-                
-                for ip_port in node["or_addresses"]:
-                    #If IPv4
-                    if ip_port[0] != '[':
-                        ip = ip_port.split(':')[0]
-                        port = int(ip_port.split(':')[1])
-                    else: # if IPv4
-                        ip = ip_port[1:].split(']')[0]
-                        port = int(ip_port[1:].split(']')[1][1:])
-                    #guard_list.setdefault(port,()).append(ip)
-                    guard_list_ports.setdefault(port,set()).add(ip)
-                    guard_set.add(ip)
+def get_all_guards_ip(nodes: list) -> set:
+    all_guard_ip = set()
+    for node in nodes:
+        if "Guard" in node["flags"]:
+            try:
+                if node["guard_probability"] != 0:
+                    for ip_port in node["or_addresses"]:
+                        ip = extract_ip_port(ip_port)["IP"]
+                        all_guard_ip.add(ip)
+            # some nodes doesnt have "guard_probability object since they have not been seen the last hour
+            except:
+                exit
+    return all_guard_ip
 
-        # some nodes doesnt have "guard_probability object since they have not been seen the last hour
-        except:
-            exit
 
+
+def get_guard_per_port(nodes: list) -> dict:
+    """
+    Parse the list of nodes given in entry
+    Return a dictionnary of all possible entry ports with their respective IPs :
+        Format : { port : [<list of IPs>], ...}
+
+    """
+    guard_list_ports = {}
+
+   # print(nodes)
+
+    for node in nodes:
+        if "Guard" in node["flags"]:
+            try:
+                if node["guard_probability"] != 0:
+                    for ip_port in node["or_addresses"]:
+                        #If IPv4
+                        if ip_port[0] != '[':
+                            ip = ip_port.split(':')[0]
+                            port = int(ip_port.split(':')[1])
+                        else: # if IPv4
+                            ip = ip_port[1:].split(']')[0]
+                            port = int(ip_port[1:].split(']')[1][1:])
+                        #guard_list.setdefault(port,()).append(ip)
+                        guard_list_ports.setdefault(port,set()).add(ip)
+            # some nodes doesnt have "guard_probability object since they have not been seen the last hour
+            except:
+                exit
+    return guard_list_ports
+
+
+
+
+#print(get_guard_per_port(relay_list))
+print(get_all_guards_ip(relay_list))
+
+"""
 #Creation of the list of all guard nodes
 filename = "./guards/all_guards_ips"
 file = open(filename,'w')
@@ -55,3 +110,4 @@ for port in guard_list_ports.keys():
     for ip in guard_list_ports[port]:
         file.write(ip + '\n')
     file.close()
+"""
