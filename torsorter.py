b79e3e45bd20dd32cdcc25a78e0ad9bafb6f3176
ndiab <nabil.diab59@gmail.com>
2023-06-09 08:07:10 +0000

    finalize the code reorganiaztion = definition of the file writing function

--- torsorter.py
+++ torsorter.py
@@ -1,4 +1,6 @@
 import requests
+import os
+
 
 ####################################
 #### GLOBAL VARIABLE DEFINITION ####
@@ -10,14 +12,13 @@ args = "details?limit=1000"
 full_url = url + '/' + args
 
 output_directory = "/srv/tornodes_lists/"
+guards_subdirectory = "guards/"
+exit_subdirectory = "exit/"
 
 raw_response = requests.get(full_url)
 response_json = dict(raw_response.json())
 
 relay_list = response_json["relays"]
-
-
-
 
 def extract_ip_port(ip_port_string: str) -> dict:
     """
@@ -41,7 +42,12 @@ def extract_ip_port(ip_port_string: str) -> dict:
     return ip_port_dict
 
 
-def get_all_guards_ip(nodes: list) -> set:
+def get_all_guards_ips(nodes: list) -> set:
+    """
+    Create a set of all probable guard of the tor network
+    Entry : list of TOR nodes (dict)
+    Return : Set of IPs
+    """
     all_guard_ip = set()
     for node in nodes:
         if "Guard" in node["flags"]:
@@ -77,32 +83,59 @@ def get_guard_per_port(nodes: list) -> dict:
                         port = extract_ip_port(ip_port)["PORT"]
                         
                         guard_list_ports.setdefault(port,set()).add(ip)
-            # some nodes doesnt have "guard_probability object since they have not been seen the last hour
+            # some nodes doesnt have "guard_probability" object since they have not been seen the last hour
             except:
                 exit
     return guard_list_ports
 
 
 
-
 #print(get_guard_per_port(relay_list))
-print(get_all_guards_ip(relay_list))
+#print(get_all_guards_ip(relay_list))
 
-"""
 #Creation of the list of all guard nodes
-filename = "./guards/all_guards_ips"
-file = open(filename,'w')
-for ip in guard_set:
-    file.write(ip + '\n')
 
-file.close()
+def write_file_all_guards_ips(all_guards_ips: set) -> None:
+    """
+    Write all guards IPs in a file
+    Entry : set of Ips
+    """
+    guards_directory = output_directory + guards_subdirectory 
+    # check if the output_directory exists
+    if not os.path.exists(guards_directory):
+        os.makedirs(guards_directory)
 
+    filename = "all_guards_ips"
+    full_path = guards_directory +  filename
 
-# Creation of the files for guard nodes per ports
-for port in guard_list_ports.keys():
-    filename = "./guards/ports/tor_guards_" + str(port)
-    file = open(filename,'w')
-    for ip in guard_list_ports[port]:
-        file.write(ip + '\n')
+    file = open(full_path,'w')
+    for ip in all_guards_ips:
+        file.write(ip + "\n")
     file.close()
-"""
+
+def write_file_guards_per_port(guard_list_port: dict) -> None:
+    """
+    Create one file per port used as entry guard
+    for each of these files, write the list of related IPs
+    """
+   
+    guards_port_directory = output_directory + guards_subdirectory + "ports/"
+
+    # check if the output_directory exists
+    if not os.path.exists(guards_port_directory):
+        os.makedirs(guards_port_directory)
+
+    for port in guard_list_port.keys():
+        filename = "tor_guards_" + str(port)
+        full_path = guards_port_directory + filename
+        file = open(full_path,'w')
+        for ip in guard_list_port[port]:
+            file.write(ip + '\n')
+        file.close()
+
+#1- write the list of all guards
+write_file_all_guards_ips(get_all_guards_ips(relay_list))
+
+#2 write all the files with list of guards per ip
+write_file_guards_per_port(get_guard_per_port(relay_list))
+
